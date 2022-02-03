4e61beb0f738cb8de2f511da1365e11ac8e37f0d
André Kuratomi <andrekuratomi@gmail.com>
2022-02-03 18:27:06 -0300

    feat: Docker updated and running ormconfig.json drafted

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import app from "./app";
+
+const PORT = 3000;
+
+createConnection()
+  .then(() => {
+    console.log("Database connected!");
+    app.listen(PORT, () => {
+      console.log("Server running!");
+    });
+  })
+  .catch((error) => console.log(error));
