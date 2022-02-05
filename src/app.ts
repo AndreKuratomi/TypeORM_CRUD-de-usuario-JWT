c4f7c481d583bd061eeb55b738adeda2153a9977
André Kuratomi <andrekuratomi@gmail.com>
2022-02-05 20:25:48 -0300

    feat: app updated with type Express and attempt to synthesize ormconfig made

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import router from "./router";
+import express, {Express} from "express";
+import router from "./routes";
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
 app.use(router);
