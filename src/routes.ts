614c21aacb48e1dee1ead9c617e7afc2231b85f8
André Kuratomi <andrekuratomi@gmail.com>
2022-02-06 19:46:04 -0300

    refactor: listUsers service cleancode

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { Router } from "express";
 
 import RegisterUserController from "./controllers/registerUser.controller";
 import LoginUserController from "./controllers/loginUser.controller";
-// import ListUsersController from "./controllers/listUsers.controller";
+import ListUsersController from "./controllers/listUsers.controller";
 // import ListUserProfileController from "./controllers/listUserProfile.controller";
 // import UpdateUserController from "./controllers/updateUser.controller";
 // import DeleteUserController from "./controllers/deleteUser.controller";
@@ -16,14 +16,14 @@ const router = Router();
 
 const registerUserController = new RegisterUserController();
 const loginUserController = new LoginUserController();
-// const listUserUserController = new ListUsersController();
+const listUsersController = new ListUsersController();
 // const listUserProfileController = new ListUserProfileController();
 // const updateUserController = new UpdateUserController();
 // const deleteUserController = new DeleteUserController();
 
 router.post("/users", registerUserController.handle);
 router.post("/login", loginUserController.handle);
-// router.get("/users", isTokenValid, isUserAdmin, listUserUserController.handle);
+router.get("/users", isTokenValid, isUserAdmin, listUsersController.handle);
 // router.get("/users/profile", isTokenValid, listUserProfileController.handle);
 // router.patch("/users/:uuid", isTokenValid, updateUserController.handle);
 // router.delete("/users/:uuid", isTokenValid, deleteUserController.handle);
