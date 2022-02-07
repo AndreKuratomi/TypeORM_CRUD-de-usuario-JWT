8aafd93bd960efb661cddc6e14d8c4a3dc9acae1
André Kuratomi <andrekuratomi@gmail.com>
2022-02-07 13:45:34 -0300

    fix: attempts to solve services that demand token

--- src/controllers/listUserProfile.controller.ts
+++ src/controllers/listUserProfile.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import ListUserProfile from "../services/listUserProfile.service";
+import { tokenFirstApproach } from "../services/token.service";
 
 class ListUserProfileController {
   async handle(request: Request, response: Response) {
+    const token = request.headers.authorization;
+    const tokenItself = tokenFirstApproach(token);
+
     const listUserProfileService = new ListUserProfile();
-
-    const userData = await listUserProfileService.execute();
-
+    const userData = await listUserProfileService.execute({ tokenItself });
+    console.log(userData);
     return response.json(userData);
   }
 }
--- src/services/listUserProfile.service.ts
+++ src/services/listUserProfile.service.ts
@@ -1,21 +1,29 @@
 /* eslint-disable quotes */
 import { getCustomRepository } from "typeorm";
+import jwt from "jsonwebtoken";
 
+import config from "../config/jwt.config";
 import UserRepository from "../repository/user.repository";
 
+interface ITokenProp {
+  tokenItself: string;
+}
+
 class ListUserProfile {
-  async execute() {
+  async execute({ tokenItself }: ITokenProp) {
     const userRepository = getCustomRepository(UserRepository);
 
-    const userProfile = await userRepository.findOne({
-      email: "daniel3@kenzie.com",
-    });
-    if (!userProfile) {
-      throw new Error("No user found!");
-      // return response.status(404).json({ message: "No user found!" });
-    }
+    const token = tokenItself;
+    jwt.verify(token, config.secret as string, async (err, decoded: any) => {
+      const tokenId = decoded.id;
+      const userProfile = await userRepository.findOne({ id: tokenId });
+      if (!userProfile) {
+        throw new Error("No user found!");
+      }
+      console.log(userProfile);
 
-    return userProfile;
+      request.userProfile = userProfile;
+    });
   }
 }
 
