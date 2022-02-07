dfcc9547b80cd579e9569f77c594f92fafff584a
André Kuratomi <andrekuratomi@gmail.com>
2022-02-07 14:52:26 -0300

    feat: New middleware for extracting Id from token created

--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,12 @@ import RegisterUserController from "./controllers/registerUser.controller";
 import LoginUserController from "./controllers/loginUser.controller";
 import ListUsersController from "./controllers/listUsers.controller";
 import ListUserProfileController from "./controllers/listUserProfile.controller";
-import UpdateUserController from "./controllers/updateUser.controller";
-import DeleteUserController from "./controllers/deleteUser.controller";
+// import UpdateUserController from "./controllers/updateUser.controller";
+// import DeleteUserController from "./controllers/deleteUser.controller";
 
 import { isTokenValid } from "./middlewares/token.middleware";
 import { isUserAdmin } from "./middlewares/admin.middleware";
+import { extractTokenId } from "./middlewares/tokenId.middleware";
 
 const router = Router();
 
@@ -18,14 +19,19 @@ const registerUserController = new RegisterUserController();
 const loginUserController = new LoginUserController();
 const listUsersController = new ListUsersController();
 const listUserProfileController = new ListUserProfileController();
-const updateUserController = new UpdateUserController();
-const deleteUserController = new DeleteUserController();
+// const updateUserController = new UpdateUserController();
+// const deleteUserController = new DeleteUserController();
 
 router.post("/users", registerUserController.handle);
 router.post("/login", loginUserController.handle);
 router.get("/users", isTokenValid, isUserAdmin, listUsersController.handle);
-router.get("/users/profile", isTokenValid, listUserProfileController.handle);
-router.patch("/users/:id", isTokenValid, updateUserController.handle);
-router.delete("/users/:id", isTokenValid, deleteUserController.handle);
+router.get(
+  "/users/profile",
+  isTokenValid,
+  extractTokenId,
+  listUserProfileController.handle
+);
+// router.patch("/users/:id", isTokenValid, updateUserController.handle);
+// router.delete("/users/:id", isTokenValid, deleteUserController.handle);
 
 export default router;
