cd0f11f22f9e69a1aa5b4ef3653736585bce039c
André Kuratomi <andrekuratomi@gmail.com>
2022-02-07 16:33:30 -0300

    feat: updateUser controller done and update middleware deleted

--- src/controllers/updateUser.controller.ts
+++ src/controllers/updateUser.controller.ts
@@ -1,16 +1,44 @@
+/* eslint-disable quotes */
+/* eslint-disable prefer-destructuring */
 import { Request, Response } from "express";
-import UpdateUserService from "../services/updateUser.service";
+import { getCustomRepository } from "typeorm";
+
+import UserRepository from "../repository/user.repository";
 
 class UpdateUserController {
-  async handle(request: Request, response: Response) {
+  async handle(request: any, response: Response) {
     const { id } = request.params;
     const data = request.body;
 
-    const updateUserService = new UpdateUserService();
+    const userRepository = getCustomRepository(UserRepository);
 
-    const updatedUser = await updateUserService.execute({ id, data });
+    for (const elem in data) {
+      console.log(elem);
+      if (elem === "isAdmin") {
+        throw new Error("'isAdmin' field cannot be updated!");
+      }
+    }
 
-    return response.json(updatedUser);
+    const userProfile = request.userProfile;
+
+    if (
+      (userProfile.isAdmin === true && userProfile.id === id) ||
+      (userProfile.isAdmin === true && userProfile.id !== id)
+    ) {
+      await userRepository.update(id, data);
+    }
+
+    if (userProfile.isAdmin === false && userProfile.id === id) {
+      await userRepository.update(id, data);
+    } else if (userProfile.isAdmin === false && userProfile.id !== id) {
+      throw new Error("Only admins may update non self-profiles!");
+    }
+
+    userProfile.updatedOn = new Date();
+
+    const user = await userRepository.findOne({ id });
+
+    return response.json(user);
   }
 }
 
--- src/services/updateUser.service.ts
+++ src/services/updateUser.service.ts
@@ -1,8 +1,5 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable quotes */
-import { getCustomRepository } from "typeorm";
-
-import UserRepository from "../repository/user.repository";
 
 interface IProps {
   id: string;
@@ -10,38 +7,7 @@ interface IProps {
 }
 
 class UpdateUserService {
-  async execute({ id, data }: IProps) {
-    const userRepository = getCustomRepository(UserRepository);
-
-    for (const elem in data) {
-      if (elem === "isAdmin") {
-        throw new Error("'isAdmin' field cannot be updated!");
-      }
-    }
-
-    const { userProfile } = request.userProfile;
-
-    // COMO DEIXAR CLARO QUE QUEM É ADMIN PODE TUDO E QUEM NÃO PODE SÓ NO PRÓPRIO???
-    if (
-      (userProfile.isAdmin === true && userProfile.id === id) ||
-      (userProfile.isAdmin === true && userProfile.id !== id)
-    ) {
-      await userRepository.update(id, data);
-      // console.log(userRepository);
-    }
-    if (userProfile.isAdmin === false && userProfile.id === id) {
-      await userRepository.update(id, data);
-      // console.log(userRepository);
-    } else if (userProfile.isAdmin === false && userProfile.id !== id) {
-      throw new Error("'isAdmin' field cannot be updated!");
-    }
-
-    const user = await userRepository.findOne({ id });
-
-    user.updatedOn = new Date();
-
-    return user;
-  }
+  async execute({ id, data }: IProps) {}
 }
 
 export default UpdateUserService;
