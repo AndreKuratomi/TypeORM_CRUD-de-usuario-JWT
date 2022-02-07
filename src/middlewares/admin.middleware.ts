6bbe8f866635fd6c57ede25fffbca062fdfe38c7
André Kuratomi <andrekuratomi@gmail.com>
2022-02-06 21:30:15 -0300

    feat: adm middleware completed

--- src/middlewares/admin.middleware.ts
+++ src/middlewares/admin.middleware.ts
@@ -1,18 +1,39 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable quotes */
+import { NextFunction, Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
+import config from "../config/jwt.config";
 import UserRepository from "../repository/user.repository";
 
-export const isUserAdmin = async (id: string) => {
+export const isUserAdmin = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
   const usersRepository = getCustomRepository(UserRepository);
 
-  const adminCandidate = await usersRepository.findOne({ id });
+  const isValidAdmin = await usersRepository.findOne({ isAdmin: true });
 
-  if (!adminCandidate) {
-    throw new Error("No user found!");
-  } else if (adminCandidate.isAdmin === true) {
-    return true;
+  // REPETITIVO
+  const auth = request.headers.authorization;
+
+  if (auth === undefined) {
+    // throw new Error("Headers unabled!");
+    return response.status(401).json({ message: "Headers unabled!" });
   }
-  return false;
+
+  const token = auth.split(" ")[1];
+
+  jwt.verify(token, config.secret as string, (err, decoded: any) => {
+    if (isValidAdmin?.email !== decoded["email"]) {
+      // throw new Error("This user is not an administrator!");
+      return response
+        .status(401)
+        .json({ message: "This user is not an administrator!" });
+    }
+  });
+
+  return next();
 };
